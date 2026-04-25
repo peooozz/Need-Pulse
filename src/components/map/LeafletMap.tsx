da75d86932ec9a2979c93b3f1224ddc3276a73dc
peooozz <thulpiyush6@gmail.com>
2026-04-26 01:41:41 +0530

    feat: replace google maps with leaflet and openstreetmap

--- src/components/map/LeafletMap.tsx
+++ src/components/map/LeafletMap.tsx
@@ -24,8 +24,13 @@ export default function LeafletMap({ needs, selectedNeedId, onNeedClick }: Leafl
       zoomControl={false}
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+        attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+        url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+      />
+      {/* City Names and Borders Layer */}
+      <TileLayer
+        url="https://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Boundaries_and_Places/MapServer/tile/{z}/{y}/{x}"
+        zIndex={2}
       />
       {needs.map((need) => {
         const cat = CATEGORY_CONFIG[need.category];
